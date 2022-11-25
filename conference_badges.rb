4b0c32dbded0cb376733ff5382b5a9893e8b22d2
Lianzi <lianziliu.sugar@gmail.com>
2022-11-24 20:26:14 -0500

    commit tree c2ba6c60ae0956cb79b2dd4425ca4b1360aafa69

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require "pry"
+
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_ary
+    name_ary.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms name_ary
+    name_ary.map { |name|
+    "Hello, #{name}! You'll be assigned to room #{ (name_ary.find_index name) + 1}!"
+    }
+end
+
+def printer name_ary
+    badges = batch_badge_creator(name_ary)
+    badges.each { |badge| puts badge }
+
+    rooms = assign_rooms(name_ary)
+    rooms.each { |room| puts room }
+end
